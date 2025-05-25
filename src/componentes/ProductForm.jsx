adea5ebc0ce00811e8b9e9111bbb8a500b0e7fb5
GuanactolayRodrigo <42583662@fi.edu.unju.ar>
2025-05-25 18:00:56 -0300

    Cambio de funcinones simples a funciones flechas.

--- src/componentes/ProductForm.jsx
+++ src/componentes/ProductForm.jsx
@@ -1,63 +1,105 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-
-function ProductForm({ onAdd, onUpdate, editingProduct }) {
-
-  
+const ProductForm = ({ onAdd, onUpdate, editingProduct }) => {
   const [product, setProduct] = useState({
-    id: '',
-    descripcion: '',
-    precioUnitario: '',
-    descuento: '',
-    stock: ''
+    id: "",
+    descripcion: "",
+    precioUnitario: "",
+    descuento: "",
+    stock: "",
   });
   const [contadorId, setContadorId] = useState(1);
 
-useEffect(() => {
+  useEffect(() => {
     if (editingProduct) {
       setProduct(editingProduct);
     } else {
-      setProduct({ id: '', descripcion: '', precioUnitario: '', descuento: '', stock: '' });
+      setProduct({
+        id: "",
+        descripcion: "",
+        precioUnitario: "",
+        descuento: "",
+        stock: "",
+      });
     }
   }, [editingProduct]);
 
-
-const handleChange = (e) => {
+  const handleChange = (e) => {
     const { name, value } = e.target;
-    setProduct(prev => ({ ...prev, [name]: value }));
+    setProduct((prev) => ({ ...prev, [name]: value }));
   };
 
-
-const handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     const precioConDescuento =
       product.precioUnitario * (1 - product.descuento / 100);
-    const newProduct = { ...product,id:+product.id, precioUnitario: +product.precioUnitario, descuento: +product.descuento, stock: +product.stock, precioConDescuento };
-   if (editingProduct) {
+    const newProduct = {
+      ...product,
+      id: +product.id,
+      precioUnitario: +product.precioUnitario,
+      descuento: +product.descuento,
+      stock: +product.stock,
+      precioConDescuento,
+    };
+    if (editingProduct) {
       onUpdate(newProduct);
     } else {
       newProduct.id = contadorId;
-      setContadorId(prev => prev + 1);
+      setContadorId((prev) => prev + 1);
       onAdd(newProduct);
     }
 
-    setProduct({ id: '', descripcion: '', precioUnitario: '', descuento: '', stock: '' });
+    setProduct({
+      id: "",
+      descripcion: "",
+      precioUnitario: "",
+      descuento: "",
+      stock: "",
+    });
   };
 
-
-
   return (
-    
-
     <form onSubmit={handleSubmit}>
-      <input className='in' name="descripcion" placeholder="Descripción" value={product.descripcion} onChange={handleChange} required />
-      <input className='in' name="precioUnitario" type="number" placeholder="Precio Unitario" value={product.precioUnitario} onChange={handleChange} required />
-      <input className='in' name="descuento" type="number" placeholder="Descuento %" value={product.descuento} onChange={handleChange} required />
-      <input className='in' name="stock" type="number" placeholder="Stock" value={product.stock} onChange={handleChange} required />
-      <button type="submit">{editingProduct ? 'Actualizar' : 'Agregar'} Producto</button>
+      <input
+        className="in"
+        name="descripcion"
+        placeholder="Descripción"
+        value={product.descripcion}
+        onChange={handleChange}
+        required
+      />
+      <input
+        className="in"
+        name="precioUnitario"
+        type="number"
+        placeholder="Precio Unitario"
+        value={product.precioUnitario}
+        onChange={handleChange}
+        required
+      />
+      <input
+        className="in"
+        name="descuento"
+        type="number"
+        placeholder="Descuento %"
+        value={product.descuento}
+        onChange={handleChange}
+        required
+      />
+      <input
+        className="in"
+        name="stock"
+        type="number"
+        placeholder="Stock"
+        value={product.stock}
+        onChange={handleChange}
+        required
+      />
+      <button type="submit">
+        {editingProduct ? "Actualizar" : "Agregar"} Producto
+      </button>
     </form>
   );
-}
+};
 
-
-export default ProductForm
+export default ProductForm;
